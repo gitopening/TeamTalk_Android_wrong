e0c17487e1379cf5eb326484dfdcc167dbf577ab
gitopening <127179317+gitopening@users.noreply.github.com>
2023-03-29 22:25:57 +0800

    日志写入文件 初始化
    日志写入文件 初始化

--- android/app/src/main/java/com/mogujie/tt/app/IMApplication.java
+++ android/app/src/main/java/com/mogujie/tt/app/IMApplication.java
@@ -1,15 +1,19 @@
 package com.mogujie.tt.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import com.mogujie.tt.imservice.service.IMService;
 import com.mogujie.tt.utils.ImageLoaderUtil;
 import com.mogujie.tt.utils.Logger;
+import com.mogujie.tt.utils.MLoggerFile;
 
 
 public class IMApplication extends Application {
 
 	private Logger logger = Logger.getLogger(IMApplication.class);
+	private Context mContext;
+
 
 	/**
 	 * @param args
@@ -21,6 +25,9 @@ public class IMApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		logger.i("Application starts");
+		mContext = this;
+		MLoggerFile.Init(mContext,true);
+
 		startIMService();
 		ImageLoaderUtil.initImageLoaderConfig(getApplicationContext());
 	}
